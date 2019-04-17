8ca665dd005641a045d71ee7c63d65d7f8b106ad
Colin Roche <G00349215@gmit.ie>
2019-04-17 21:34:13 +0100

    recipes

--- MobileAppProject/MobileAppProject/PageViews/Home.xaml.cs
+++ MobileAppProject/MobileAppProject/PageViews/Home.xaml.cs
@@ -11,7 +11,7 @@ namespace MobileAppProject
 		public Home ()
 		{
 			InitializeComponent ();
-		}
+        }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
--- MobileAppProject/MobileAppProject/obj/Debug/netstandard2.0/PageViews/Home.xaml.g.cs
+++ MobileAppProject/MobileAppProject/obj/Debug/netstandard2.0/PageViews/Home.xaml.g.cs
@@ -17,8 +17,12 @@ namespace MobileAppProject {
     public partial class Home : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Grid PageContentGrid;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Home));
+            PageContentGrid = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Grid>(this, "PageContentGrid");
         }
     }
 }
