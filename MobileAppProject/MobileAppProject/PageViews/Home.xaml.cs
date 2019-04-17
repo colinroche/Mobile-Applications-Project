b3c17421cc8838b1001061270c2b1dfa51bc216c
Colin Roche <G00349215@gmit.ie>
2019-04-17 23:49:51 +0100

    finishes

--- MobileAppProject/MobileAppProject/PageViews/Home.xaml.cs
+++ MobileAppProject/MobileAppProject/PageViews/Home.xaml.cs
@@ -1,27 +1,46 @@
 ﻿using MobileAppProject.PageViews;
 using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace MobileAppProject
 {
 	public partial class Home : ContentPage
 	{
-		public Home ()
+        Label orientationLabel;
+
+        public Home ()
 		{
+            // attempt on setting orientation
+            orientationLabel = new Label();
+            orientationLabel.VerticalOptions = LayoutOptions.Center;
+            orientationLabel.HorizontalOptions = LayoutOptions.Center;
+            Content = orientationLabel;
+
 			InitializeComponent ();
         }
 
+        // Method to store page orientation
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            // call the base
+            base.OnSizeAllocated(width, height);
+
+            // check dimensions to deteremine screen orientation
+            if (width > height)
+                Debug.WriteLine("Landscape");
+            else
+                Debug.WriteLine("Portrait");
+        }
+
+        // Button clicks navigate to new page
         private void Button_Clicked(object sender, EventArgs e)
         {
             var page = new Breakfast();
             //NavigationPage.SetHasNavigationBar(page, true);
             //NavigationPage.SetBackButtonTitle(page, "Back");
-
-
             Navigation.PushAsync(page);
-
             //NavigationPage.SetHasBackButton(page, true);
 
         }
